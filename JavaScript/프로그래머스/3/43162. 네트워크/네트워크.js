657615b1fccfe18cb58401f936fd99dff8f4bda7
유선향 <gbtmxlf@naver.com>
2025-12-10 21:35:39 +0900

    [level 3] Title: 네트워크, Time: 0.60 ms, Memory: 33.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/43162. 네트워크/네트워크.js
+++ JavaScript/프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,20 +1,22 @@
 function solution(n, computers) {
-    const visited = Array(n).fill(false)
-    let count = 0 
+    let count = 0
+    const visited =Array(n).fill(false)
     
-    const dfs = (el)=> {
-        visited[el] = true
-        for (let i = 0; i<n;i++){
-            if (computers[el][i]===1 && !visited[i]){
+    const dfs = (node) => {
+        visited[node] = true
+        for (let i = 0 ; i<n ; i++) {
+            if (!visited[i]&& computers[node][i]===1){
                 dfs(i)
             }
         }
     }
-    for (let i=0 ; i<n; i++){
-        if (!visited[i]){
+    
+    for (let i = 0; i<n ; i++) {
+        if (!visited[i]) {
             dfs(i)
             count++
         }
+        
     }
-    return count
+    return count;
 }
