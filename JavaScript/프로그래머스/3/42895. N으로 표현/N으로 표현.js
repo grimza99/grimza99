aaecd309893aa4cb3bff0eb6cf620ce9f98c7f94
유선향 <gbtmxlf@naver.com>
2025-12-16 19:15:22 +0900

    [level 3] Title: N으로 표현, Time: 17.16 ms, Memory: 39.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/42895. N으로 표현/N으로 표현.js
+++ JavaScript/프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -1,25 +1,22 @@
 function solution(N, number) {
-  if (N === number) return 1; 
-
-  const dp = Array.from({ length: 9 }, () => new Set());
-
-  for (let c = 1; c <= 8; c++) {
-    const concat = Number(String(N).repeat(c));
-    dp[c].add(concat);
-
-    for (let i = 1; i < c; i++) {
-      for (const a of dp[i]) {
-        for (const b of dp[c - i]) {
-          dp[c].add(a + b);
-          dp[c].add(a - b);
-          dp[c].add(a * b);
-          if (b !== 0) dp[c].add(Math.trunc(a / b));
+    if (N ===number) return 1
+    
+    const dp = Array.from({length: 9},()=>new Set())
+    
+    for (let i = 1 ; i<= 8 ; i++) {
+        dp[i].add(Number(String(N).repeat(i)))
+        
+        for (let j = 1 ; j<i; j++) {
+            for (const a of dp[j] ) {
+                for (const b of dp[i-j]) {
+                    dp[i].add(a+b);
+                    dp[i].add(a-b);
+                    dp[i].add(a*b);
+                    if (b !== 0) dp[i].add(Math.trunc(a/b));
+                }
+            }
         }
-      }
+        if (dp[i].has(number)) return i
     }
-
-    if (dp[c].has(number)) return c;
-  }
-
-  return -1;
+    return -1
 }
